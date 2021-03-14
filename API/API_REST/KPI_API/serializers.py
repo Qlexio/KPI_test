c71334e7a94b3dbaff44053e300330614009f43a
Qlexio <benard.al@gmail.com>
2021-03-14 14:20:50 +0100

    Create serializer and add dataset to db

---
+++ API/API_REST/KPI_API/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import JsonDataset
+
+class JsonDatasetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model= JsonDataset
+        fields= ["titreoperation", "entreprise", "annee_de_livraison", "ville", "mandataire", "ppi", "lycee",
+            "notification_du_marche", "codeuai", "longitude", "etat_d_avancement", "montant_des_ap_votes_en_meu",
+            "cao_attribution", "latitude", "maitrise_d_oeuvre", "mode_de_devolution", "annee_d_individualisation",
+            "enveloppe_prev_en_meu",]
