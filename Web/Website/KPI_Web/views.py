9223effcffe39234a601acec0814e004631f355c
Qlexio <benard.al@gmail.com>
2021-03-16 14:41:16 +0100

    End addition of pagination to views functions

--- Web/Website/KPI_Web/views.py
+++ Web/Website/KPI_Web/views.py
@@ -69,19 +69,34 @@ def investments_by_city(request):
 
 def investments_by_progress(request):
 
+    if not request.method == "POST":
+        if "page" not in request.get_full_path():
+            request.session["entry"] = ""
+        else:
+            request.POST = request.session["entry"]
+            request.method = "POST"
+
     if request.method == "POST":
 
         context = {}
 
         form = InvestmentsByCity(request.POST)
+        request.session["entry"] = request.POST
+
         if form.is_valid():
             progress = form.cleaned_data["entry"]
             url = "http://127.0.0.1:8000/investments/etat/" + progress
             datas = r.get(url)
 
-            if len(datas.json()) > 0:
-                context["datas"] = datas.json()
-                return render(request, "KPI_Web/listing.html", context)
+            json_datas = datas.json()
+            # Pagination
+            paginator = Paginator(json_datas, 5)
+            page_number = request.GET.get("page")
+            page_obj = paginator.get_page(page_number)
+
+            if len(json_datas) > 0:
+                
+                return render(request, "KPI_Web/listing.html", context={"datas": page_obj})
             else:
                 context["form"] = InvestmentsByCity()
                 context["bool"] = True
@@ -95,19 +110,34 @@ def investments_by_progress(request):
 
 def investments_by_code(request):
 
+    if not request.method == "POST":
+        if "page" not in request.get_full_path():
+            request.session["entry"] = ""
+        else:
+            request.POST = request.session["entry"]
+            request.method = "POST"
+
     if request.method == "POST":
 
         context = {}
 
         form = InvestmentsByCity(request.POST)
+        request.session["entry"] = request.POST
+
         if form.is_valid():
             code = form.cleaned_data["entry"]
             url = "http://127.0.0.1:8000/investments/code/" + code
             datas = r.get(url)
 
-            if len(datas.json()) > 0:
+            json_datas = datas.json()
+            # Pagination
+            paginator = Paginator(json_datas, 5)
+            page_number = request.GET.get("page")
+            page_obj = paginator.get_page(page_number)
+
+            if len(json_datas) > 0:
                 context["datas"] = datas.json()
-                return render(request, "KPI_Web/listing.html", context)
+                return render(request, "KPI_Web/listing.html", context={"datas": page_obj})
             else:
                 context["form"] = InvestmentsByCity()
                 context["bool"] = True
@@ -121,20 +151,35 @@ def investments_by_code(request):
 
 def investments_by_city_progress(request):
 
+    if not request.method == "POST":
+        if "page" not in request.get_full_path():
+            request.session["entry"] = ""
+        else:
+            request.POST = request.session["entry"]
+            request.method = "POST"
+
     if request.method == "POST":
 
         context = {}
 
         form = InvestmentsByCityProgress(request.POST)
+        request.session["entry"] = request.POST
+        
         if form.is_valid():
             city = form.cleaned_data["entry"]
             progress = form.cleaned_data["entry2"]
             url = "http://127.0.0.1:8000/investments/ville_etat/" + city + "+" + progress
             datas = r.get(url)
 
-            if len(datas.json()) > 0:
-                context["datas"] = datas.json()
-                return render(request, "KPI_Web/listing.html", context)
+            json_datas = datas.json()
+            # Pagination
+            paginator = Paginator(json_datas, 5)
+            page_number = request.GET.get("page")
+            page_obj = paginator.get_page(page_number)
+
+            if len(json_datas) > 0:
+                
+                return render(request, "KPI_Web/listing.html", context={"datas": page_obj})
             else:
                 context["form"] = InvestmentsByCityProgress()
                 context["bool"] = True
